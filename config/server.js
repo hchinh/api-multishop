c83ee12062597cbcdb60a50c1143da4a97354166
Huynh Chinh <huynhchinhrvp10@gmail.com>
2021-07-09 17:29:58 +0700

    push

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '0f6480ac3392348097de7c7b37d0a897'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'f27496e1-0ee3-4560-8393-4f1c607e859a'
+};
